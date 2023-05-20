852a2fee458afbcd71a526697c21556698207e4f
marianelopes <104696075+Marianelopes84@users.noreply.github.com>
2023-05-19 21:39:20 -0300

    Create Condicional.java

---
+++ Condicional.java
@@ -0,0 +1,16 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 1990;
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "normal";
+
+// código omitido
+
+        if (incluidoNoPlano == true || tipoPlano.equals("plus")){
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação");
+        }
+    }
+}
